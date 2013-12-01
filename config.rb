6aab98c77e5521e9c02c001c21a845b058812193
Eleazar Meza <meza.eleazar@gmail.com>
2013-12-01 04:45:09 -0430

    Agregados archivos de tipografia Aller. Simplificado config.rb

--- config.rb
+++ config.rb
@@ -1,18 +1,9 @@
-http_path = "/"
-css_dir = "core/css"
+relative_assets = true
+css_dir = "."
 sass_dir = "core/scss"
 images_dir = "core/images"
 javascripts_dir = "core/js"
+fonts_dir = "core/fonts/"
 
 environment = :development
 output_style = :compressed
-
-require 'fileutils'
-on_stylesheet_saved do |file|
-  filename = File.basename(file)
-  newdir = File.dirname(file) + "/../../"
-  if File.exists?(file) && filename == "style.css"
-    puts "Moving: #{filename}"
-    FileUtils.mv file, newdir
-  end
-end
